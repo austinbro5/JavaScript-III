edde146568c03c467c4f7b42cabc46d6da1e0520
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-04-03 16:59:54 -0500

    finshed and testing

--- assignments/this.js
+++ assignments/this.js
@@ -1,73 +1,58 @@
+import Const from 'Const';
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window/ Global Object Binding
-    If the scope of 'this' is left unspecified, the default scope will be global and the default value of 'this' will be the window/console Object. 
-* 2. Implicit Binding
-    If you create or work with an object with functions, and then proceed to call that function using dot notation, 'this' is the object the precedes the dot and function call. 
-* 3. New binding
-    This principle comes up once we are working with objects created by constructor functions. When an object is made using a constructor function, and we call that object using dot notation, the 'this' key word refers to the specific instance of the object created using our constructor, NOT the constructor itself. 
-* 4. Explicit binding
-    Explicit binding occurs whenever we use the call or apply method. When we use either of these keywords, 'this' is set to the object passed in as an argument to call or apply. 
+* 1. Implicit Binding: You look at the left pf the dot at call time
+* 2. Explicit Binding: Telling the function what the context of this keyword is going to be- 
+using call, apply or bind
+* 3. New Binding: When you have function invoke with a new keyword is bound to new object
+being constructed
+* 4.  Window Binding: If none of the rules apply then  this keyword is going to default
+to the window object unless you're  in strict mode then it will just be undefined. 
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Window Binding example
 
-// code example for Window Binding
+// 
 
-// function sayHello(hello){
-//     console.log(this);
-//     return hello;
-// }
+// Implicit Binding example - 
 
-// sayHello("Hello");
+const me ={
+    name: 'Mindy',
+    Age: 34,
+    sayName: function(){
+            console.log(this.name);
+    }
+};
 
-// Principle 2
+me.sayName();  
 
-// code example for Implicit Binding
+// when you call a function/function is invoked look to left of dot
+//this keyword will reference whats to the left of the dot
 
-// const newObj = {
-//     word: 'Hello',
-//     speak: function(input) {
-//         console.log(`${this.word}, ${input}.`);
-//         console.log(this);
-//     }
-// };
+// New Binding Example
 
-// newObj.speak('World');
-
-// Principle 3
-
-// code example for New Binding
-
-// function Dancer(name) {
-//     this.question = "Would you like to dance"
-//     this.name = name;
-//     this.ask = function(partner) {
-//         console.log(`I, ${this.name}, want to ask, "${this.question}," ${partner} ?`);
-//     };
-// }
-
-// const John = new Dancer('John');
-// John.ask("Jane");
+Const Animal = function (color, name, type) {
+    this.color = color;
+    this.name = name;
+    this.type = type;
+}
+const zebra = new Animal ('black and white', 'zorro', 'zebra');
 
 
-// Principle 4
+// Explicit Binding example
 
-// code example for Explicit Binding
+const sayName = function(Book1, Book2, Book3){
+        console.log('My name is' + this.name + 'and I love' + Book1 + ',' + Book2 + ',and ' + Book3);
+};
 
-// function Dancer(name) {
-//     this.question = "Would you like to dance"
-//     this.name = name;
-//     this.ask = function(partner) {
-//         console.log(`I, ${this.name}, want to ask, "${this.question}," ${partner} ?`);
-//     };
-// }
+const Samuel = {
+    name: 'Samuel',
+    age: 34
+};
 
-// const John = new Dancer('John');
-// John.ask("Jane");
+const books =['Math', 'History', 'Science'];
 
-// const Jane = new Dancer('Jane');
-// John.ask.call(Jane, "Mary");
+sayName.apply(Samuel, books);
